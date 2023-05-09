c1d3f76d7d8b82ae4bd329030d25c61c84480db0
InaSofijaDeren <InaSofijaDeren@LAPTOP-OU2E00DH>
2023-05-09 18:37:06 +0300

    Add project files.

---
+++ DemoIsvedimas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoIsvedimas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Labas, pasauli");
+            Console.WriteLine();
+
+            Console.Write("Manougis :");
+            Console.WriteLine(1.91);
+
+            float tecioUgis = 1.92f;
+            Console.Write("mano tecio ugis: ");
+            Console.WriteLine(tecioUgis);
+            Console.WriteLine();
+
+            int amzius = 15;
+            float ugis = 1.77f;
+
+            Console.WriteLine("Labas, man yra 15 metu ir mano ugis yra 1.77 metro");
+            Console.WriteLine("Labas, man yra {0} metu ir mano ugis yra {1} metro", amzius, ugis);
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ DemoKintamiejiN/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKintamiejiN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Komentaras 
+            /*
+             * 
+             * 
+             */
+            int skaicius; // be reiksmes
+            int skaiciusSuReiksme = 7; // su nustatyta reiksme
+            int skaiciusIsKitoKintamojo = skaiciusSuReiksme; // reiksme is kito kintamojo
+
+            //trupmeniniai 
+            float ugis = 1.77f;
+            float ugisSveikasis = 2f;
+            double ugisTikslesnis=1.66666f;
+
+            // simboliniai
+            char simbolis = 's'; // suteikiam pradine reiksme
+            simbolis = 'f';
+            char kitasSimbolis = simbolis;
+
+            string tekstas = "laba" +
+                "pagaliau";
+
+            //loginiai
+            bool arlyja = false;
+            bool arPavasaris = true;
+
+
+
+
+
+        }
+    }
+}
