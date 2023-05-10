6c78b9de5d2653a9197c9c9b64d2037c71a3730c
InaSofijaDeren <ina.sofija@gmail.com>
2023-05-10 09:19:11 +0300

    Add uzduotis03

---
+++ Uzduotis03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Uzduotis03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char tarpas = ' ';
+            Console.WriteLine("---------------------------");
+            Console.Write('|');
+            Console.Write($"{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}");
+            Console.WriteLine('|');
+            Console.Write('|');
+            Console.Write($"{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}");
+            Console.WriteLine('|');
+            Console.Write('|');
+            Console.Write($"{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}");
+            Console.Write("MANO, VARDAS");
+            Console.Write($"{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}");
+            Console.WriteLine('|');
+            Console.Write('|');
+            Console.Write($"{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}");
+            Console.Write("INA");
+            Console.Write($"{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}");
+            Console.WriteLine('|');
+            Console.Write('|');
+            Console.Write($"{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}");
+            Console.WriteLine('|');
+            Console.Write('|');
+            Console.Write($"{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}{tarpas}");
+            Console.WriteLine('|');
+            Console.WriteLine("---------------------------");
+        }
+    }
+}
